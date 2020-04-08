201f81f018b0783e667795c0a927a72fba15ec5a
Freyb <balazs.frey@gmail.com>
2020-04-08 16:41:08 +0200

    Change group seed to use json data

--- seeds/groups.ts
+++ seeds/groups.ts
@@ -1,22 +1,15 @@
 import * as Knex from "knex";
+import groupnames from "../src/util/groupnames.json";
 
 export async function seed(knex: Knex): Promise<any> {
+  const data = groupnames.names.map((name) => {
+    return { name };
+  });
   // Deletes ALL existing entries
   return knex("groups")
     .del()
     .then(() => {
       // Inserts seed entries
-      return knex("groups").insert([
-        { name: "AC Stúdió & Live" },
-        { name: "Budavári Schönherz Stúdió" },
-        { name: "HA5KFU" },
-        { name: "LEGO Kör" },
-        { name: "Kir-Dev" },
-        { name: "Menedzsment Kör" },
-        { name: "Schönherz Design Stúdió" },
-        { name: "Schönherz Elektronikai Műhely" },
-        { name: "SPOT Fotókör" },
-        { name: "Simonyi Károly Szakkollégium" },
-      ]);
+      return knex("groups").insert(data);
     });
 }
