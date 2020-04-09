32edbbcaf4ac0d3cbe45b35b26e98dde23c7f2d2
Freyb <balazs.frey@gmail.com>
2020-04-09 17:27:38 +0200

    Implement GroupLead handler middlewares

--- src/util/authentication.ts
+++ src/util/authentication.ts
@@ -22,3 +22,38 @@ export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
     next(createError(403));
   }
 };
+
+export const isGroupLead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const groups = (
+    await User.relatedQuery("leader").for((req.user as User)?.id)
+  ).map((group) => group.id);
+  console.log(groups);
+  if (groups.indexOf(parseInt(req.params.id)) > -1) {
+    next();
+  } else {
+    next(createError(403));
+  }
+};
+
+export const isAdminOrGroupLead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const groups = (
+    await User.relatedQuery("leader").for((req.user as User)?.id)
+  ).map((group) => group.id);
+  console.log(groups);
+  if (
+    groups.indexOf(parseInt(req.params.id)) > -1 ||
+    (req.user as User)?.admin
+  ) {
+    next();
+  } else {
+    next(createError(403));
+  }
+};
