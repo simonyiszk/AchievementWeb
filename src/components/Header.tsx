a50e0aaa5ab263a8a191a25675cd6f93559d2b60
Freyb <balazs.frey@gmail.com>
2020-04-15 15:33:22 +0200

    Create Header component, add logo to it

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { Box, Image } from '@chakra-ui/core';
+import React from 'react';
+
+import logoSmall from '../assets/simonyi_white_small.svg';
+import logo from '../assets/simonyi_white_white.svg';
+
+function Header(): JSX.Element {
+  return (
+    <Box p={1} backgroundColor="simonyi">
+      <Image
+        display={['none', null, 'block']}
+        height="3rem"
+        src={logo}
+        alt="Logo"
+      />
+      <Image
+        display={['block', null, 'none']}
+        height="3rem"
+        m={1}
+        src={logoSmall}
+        alt="Logo"
+      />
+    </Box>
+  );
+}
+
+export default Header;
