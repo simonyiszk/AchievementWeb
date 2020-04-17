523c335e841a29d66855f49245400c741dcfbdf2
Freyb <balazs.frey@gmail.com>
2020-04-17 19:50:21 +0200

    Add Gatsby Link to MainGroupBox

--- src/components/MainGroupBox.tsx
+++ src/components/MainGroupBox.tsx
@@ -1,31 +1,33 @@
 import { Flex, Image } from '@chakra-ui/core';
+import { Link } from 'gatsby';
 import React from 'react';
 
 interface Group {
+  id: number;
   name: string;
   img: string;
 }
 
 interface Props {
   group: Group;
-  onClick: (name: string) => void;
 }
 
-export default function MainGroupBox({ group, onClick }: Props): JSX.Element {
+export default function MainGroupBox({ group }: Props): JSX.Element {
   return (
-    <Flex
-      justify="center"
-      alignItems="center"
-      m={4}
-      p={4}
-      height="10rem"
-      width="15rem"
-      border="solid 1px rgba(0, 0, 0, 0.05)"
-      cursor="pointer"
-      boxShadow="0px 4px 12px rgba(0, 0, 0, 0.1)"
-      onClick={(): void => onClick(group.name)}
-    >
-      <Image height="100%" src={group.img} />
+    <Flex m={4}>
+      <Link to={`/group/${group.id}`}>
+        <Flex
+          justify="center"
+          alignItems="center"
+          p={4}
+          height="10rem"
+          width="15rem"
+          border="solid 1px rgba(0, 0, 0, 0.05)"
+          boxShadow="0px 4px 12px rgba(0, 0, 0, 0.1)"
+        >
+          <Image height="100%" src={group.img} />
+        </Flex>
+      </Link>
     </Flex>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,51 +3,33 @@ import React, { useEffect, useState } from 'react';
 
 import { Layout } from '../components/Layout';
 import MainGroupBox from '../components/MainGroupBox';
-import consts from '../data/consts.yml';
 import data from '../data/groups.yml';
 
 interface GroupData {
+  id: number;
   name: string;
   img: string;
 }
 interface GroupIds {
   [key: string]: number;
 }
-interface GroupFetchData {
-  id: number;
-  name: string;
-}
 
 export default function IndexPage(): JSX.Element {
   const [groupIds, setGroupIds] = useState<GroupIds>({});
 
   useEffect(() => {
-    fetch(`${consts.root_url}/groups`, {
-      mode: 'cors',
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        const newGroupIds = res.reduce(
-          (acc: GroupIds, curr: GroupFetchData) => {
-            acc[curr.name] = curr.id;
-            return acc;
-          },
-          {},
-        );
-        setGroupIds(newGroupIds);
-      })
-      .catch((err) => console.log('error', err));
+    const newGroupIds = data.reduce((acc: GroupIds, curr: GroupData) => {
+      acc[curr.name] = curr.id;
+      return acc;
+    }, {});
+    setGroupIds(newGroupIds);
   }, []);
-
-  const handleClick = (name: string): void => {
-    console.log(groupIds[name]);
-  };
 
   return (
     <Layout>
       <Flex flexWrap="wrap" justify="center">
         {data.map((group: GroupData) => (
-          <MainGroupBox key={group.name} group={group} onClick={handleClick} />
+          <MainGroupBox key={group.name} group={group} />
         ))}
       </Flex>
     </Layout>
