32d887a168641b5d20953dd00d876d565ad648fc
Freyb <balazs.frey@gmail.com>
2020-04-18 09:53:04 +0200

    Implement ShowGroupPage

--- src/pages-dynamic/show.tsx
+++ src/pages-dynamic/show.tsx
@@ -1,20 +1,66 @@
-import { Flex } from '@chakra-ui/core';
+import { Box, Flex } from '@chakra-ui/core';
 import { RouteComponentProps } from '@reach/router';
 import React, { useEffect, useState } from 'react';
 
+import AchievementBox from '../components/AchievementBox';
 import { Layout } from '../components/Layout';
+import consts from '../data/consts.yml';
+import texts from '../data/texts.yml';
+
+interface Achievement {
+  id: number;
+  title: string;
+  category: 'szakmai' | 'kozossegi';
+  level: number;
+}
+interface UserLevel {
+  id: number;
+  level: number;
+}
+interface LevelText {
+  level: number;
+  text: string;
+}
 
 interface Props extends RouteComponentProps {
   id?: string;
 }
 
 function ShowGroupPage({ id }: Props): JSX.Element {
+  const [achievements, setAchievements] = useState<Achievement[]>([]);
+  const [userlevels, setUserlevels] = useState<UserLevel[]>([]);
+
   useEffect(() => {
-    console.log(id);
-  });
+    fetch(`${consts.root_url}/groups/${id}`, {
+      mode: 'cors',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setAchievements(res.allAchievements);
+        setUserlevels(res.userAchievementLevels);
+      })
+      .catch((err) => console.log('error', err));
+  }, [id]);
   return (
     <Layout>
-      <Flex>Hello World from show page</Flex>
+      {texts.levels.map((level: LevelText) => {
+        return (
+          <Box key={level.level}>
+            <Box>{level.text}</Box>
+            <Flex flexWrap="wrap">
+              {achievements
+                .filter((achievement) => achievement.level === level.level)
+                .map((achievement: Achievement) => (
+                  <AchievementBox
+                    key={achievement.id}
+                    achievement={achievement}
+                  />
+                ))}
+            </Flex>
+          </Box>
+        );
+      })}
     </Layout>
   );
 }
