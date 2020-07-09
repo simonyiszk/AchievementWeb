2c603e6af0f42e00eb9e6096890b1070581ee102
Freyb <balazs.frey@gmail.com>
2020-07-09 23:21:34 +0200

    Implement middlewares to check achievement groups

--- apps/backend/src/app/util/authentication.ts
+++ apps/backend/src/app/util/authentication.ts
@@ -1,7 +1,9 @@
-import { Request, Response, NextFunction } from "express";
-import createError from "http-errors";
+import { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
 
-import { User } from "../components/user/user";
+import { User } from '../components/user/user';
+import { Achievement } from '../components/achievement/achievement';
+import { Completion } from '../components/completion/completion';
 
 export const isAuthenticated = (
   req: Request,
@@ -29,10 +31,10 @@ export const isGroupLead = async (
   next: NextFunction
 ) => {
   const groups = (
-    await User.relatedQuery("leader").for((req.user as User)?.id)
+    await User.relatedQuery('leader').for((req.user as User)?.id)
   ).map((group) => group.id);
   console.log(groups);
-  if (groups.indexOf(parseInt(req.params.id)) > -1) {
+  if (groups.indexOf(parseInt(req.params.groupid)) > -1) {
     next();
   } else {
     next(createError(403));
@@ -45,15 +47,55 @@ export const isAdminOrGroupLead = async (
   next: NextFunction
 ) => {
   const groups = (
-    await User.relatedQuery("leader").for((req.user as User)?.id)
+    await User.relatedQuery('leader').for((req.user as User)?.id)
   ).map((group) => group.id);
   console.log(groups);
   if (
-    groups.indexOf(parseInt(req.params.id)) > -1 ||
+    groups.indexOf(parseInt(req.params.groupid)) > -1 ||
     (req.user as User)?.admin
   ) {
     next();
   } else {
     next(createError(403));
+  }
+};
+
+export const checkGroupByAchiid = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const achievement = await Achievement.query().findOne({
+    id: parseInt(req.params.achiid),
+  });
+  if (!achievement) {
+    next(createError(404));
+  } else if (achievement.groupId != parseInt(req.params.groupid)) {
+    next(createError(403));
+  } else {
+    next();
+  }
+};
+
+export const checkGroupByUserAchiid = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const completion = await Completion.query().findOne({
+    id: parseInt(req.params.userachiid),
+  });
+  if (!completion) {
+    next(createError(404));
+  } else {
+    const achievement = await Achievement.query().findOne({
+      id: completion.achievementId,
+    });
+    if (!achievement) {
+      next(createError(404));
+    } else if (achievement.groupId != parseInt(req.params.groupid)) {
+      next(createError(403));
+    }
+    next();
   }
 };
